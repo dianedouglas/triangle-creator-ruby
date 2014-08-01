8447790e160e393ff2d44a7b31e1a43fd39a9cff
Amoreena Hagedorn & Diane Douglas <student@epicodus.com>
2014-08-01 11:45:07 -0700

    create a user interface with a main menu and exit option.

--- triangle_ui.rb
+++ triangle_ui.rb
@@ -1,7 +1,23 @@
-require "lib/triangle"
+require "./lib/triangle.rb"
 
 def main_menu
   loop do
-
+    puts "Hello, you beautiful bastard! Would you like to make a triangle? y/n"
+    response = gets.chomp
+    if response == "y"
+      puts "Please enter side one:"
+      side1 = gets.chomp.to_i
+      puts "Please enter side two:"
+      side2 = gets.chomp.to_i
+      puts "Please enter side three:"
+      side3 = gets.chomp.to_i
+      triangle = Triangle.new(side1, side2, side3)
+      puts triangle.compute_type
+    else
+      puts "Then what do you need me for...quitting"
+      exit
+    end
   end
 end
+
+main_menu
